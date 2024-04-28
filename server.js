91700067beb60a4a286376c2dc67eb5986896a72
harshal <proskylord69@gmail.com>
2024-04-28 08:19:39 +0530

    committed

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
 
-  app.use(cors(
-    {
-      origin:{"https://deploy-mern-1whq.vercel.app"},
-      methods:["POST","GET"],
-      credentials:true
-    }
-    ));
+  app.use(cors());
 
 
 
